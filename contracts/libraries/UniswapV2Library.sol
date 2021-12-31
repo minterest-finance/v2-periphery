16a8dd7d8775991c22c2184dafa61f4236ed8a3b
mishanalbandyan <misha@minterst.com>
2021-12-31 13:19:17 +0400

    Change hash

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'b594b680ac8ffbcd817033d8b9a0f18807d93811006cb8a3ad1884015bb00ea0' // init code hash
             ))));
     }
 
